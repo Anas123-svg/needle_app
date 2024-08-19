66f57f75c16037013da4753c935aed1c1488ce26
Mehran Hamza <Mehranhamza8@gmail.com>
2024-08-19 21:16:40 +0500

    new route

--- needle_client/lib/routes/app_pages.dart
+++ needle_client/lib/routes/app_pages.dart
@@ -2,9 +2,9 @@ import 'package:get/get.dart';
 import 'package:needle/routes/app_routes.dart';
 import 'package:needle/screens/splash_screen.dart';
 
-
 class AppPages {
   static final appRoutes = [
+    GetPage(name: Routes.splash, page: () => const SplashScreen()),
     GetPage(name: Routes.splash, page: () => const SplashScreen()),
   ];
 }
